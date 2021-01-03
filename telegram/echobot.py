489562d6a68cc6de1c7bee651782631888921106
Sergey Khlebnikov <hleserg@gmail.com>
2021-01-03 06:22:10 +0300

    asda

--- telegram/echobot.py
+++ telegram/echobot.py
@@ -68,7 +68,7 @@ def main():
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
-    updater.start_webhook(,,url_path='https://hleserg-bot1.herokuapp.com/')
+    updater.start_webhook(listen='127.0.0.1',port=80,url_path='https://hleserg-bot1.herokuapp.com/')
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
